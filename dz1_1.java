467d78ffcbea2a0b006bf028d127d67c71334132
Роман Бухмистов <rrb13@mail.ru>
2022-11-28 23:15:36 +0300

    Записаны ДЗ

--- dz1_1.java
+++ dz1_1.java
@@ -1,2 +1,42 @@
 
 // Вычислить n-ое треугольного число(сумма чисел от 1 до n), n! (произведение чисел от 1 до n)
+import java.util.List;
+import java.util.Scanner;
+import java.util.ArrayList;
+
+public class dz1_1 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Введите размер треугольника");
+        int r = input.nextInt();
+        nNumberSum(r);
+        nNumberMult(r);
+
+    }
+
+    public static void nNumberSum(int n) {
+        List<Integer> integers = new ArrayList<>();
+        System.out.println("\nТреугольник суммы чисел");
+        for (int i = 2; i <= n + 1; i++) {
+            int t = i - 1;
+            integers.add(t);
+            for (int value : integers) {
+                System.out.print(value + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void nNumberMult(int n) {
+        List<Integer> integers = new ArrayList<>();
+        System.out.println("\nТреугольник произведения чисел");
+        for (int i = 1; i <= n; i++) {
+            int t = i * i;
+            integers.add(t);
+            for (int value : integers) {
+                System.out.print(value + " ");
+            }
+            System.out.println();
+        }
+    }
+}
--- dz1_2.java
+++ dz1_2.java
@@ -1,3 +1,21 @@
 // Вывести все простые числа от 1 до 1000
 
+public class dz1_2 {
+    public static void main(String[] args) {
+        // Первые простые числа это — 2,3,5,7
 
+        System.out.print("2, 3, ");
+        boolean flag;
+        for (int i = 5; i <= 1000; i++) {
+            flag = true;
+            for (int j = 2; j < i / 2; j++) {
+                if ((i % j) == 0) {
+                    flag = false;
+                }
+            }
+            if (flag == true) {
+                System.out.print(i + ", ");
+            }
+        }
+    }
+}
